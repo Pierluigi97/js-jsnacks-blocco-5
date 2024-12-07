eb651fdff22896d693091b58437c481b28ea5717
Pierluigi97 <barsanofiopierluigimoretto@gmail.com>
2024-12-07 12:56:56 +0100

    snack-5: exercise complete

--- ex-js-array-methods-main/snack-4/script.js
+++ ex-js-array-methods-main/snack-4/script.js
@@ -7,3 +7,6 @@ const posts = [
 
 // Crea un nuovo array coi nomi di tutti gli autori
 // Risultato: ['Marco', 'Luca', 'Fabrizio', 'Enrico']
+
+const nomiAutori = posts.map(autore => autore.author);
+console.log(nomiAutori);
